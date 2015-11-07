44b92eb7cf6c335c404ee186206f0954bc052a6a
prabhakarmdu <prabhakarmdu@gmail.com>
2015-11-07 22:36:33 +0530

    feat:log implementation

---
+++ TestLog.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+require_once 'KLogger.php';
+$log = new KLogger ( "log.txt" , KLogger::DEBUG );
+ 
+// Do database work that throws an exception
+$log->LogError("An exception was thrown in ThisFunction()");
+ 
+// Print out some information
+$log->LogInfo("Internal Query Time: $time_ms milliseconds");
+ 
+// Print out the value of some variables
+$log->LogDebug("User Count: $User_Count");
+?>
